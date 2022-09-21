090170a237865dd0f0b2a5e1ba047743734ed008
Jose Antonio Solis Martinez <jose-antonio.solis@atos.net>
2022-09-21 15:39:07 -0500

    add class to hold header and other common elements

--- src/main/java/com/finalproject/qa/pageobjects/CreateAccountPage.java
+++ src/main/java/com/finalproject/qa/pageobjects/CreateAccountPage.java
@@ -5,11 +5,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class HomePage extends CommonElements {
-
+public class CreateAccountPage extends CommonElements {
     WebDriver driver;
 
-    public HomePage(WebDriver driver) {
+    public CreateAccountPage(WebDriver driver) {
         super(driver);
         this.driver = driver;
         PageFactory.initElements(driver, this);
