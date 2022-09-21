9cd057f8f8ffc147ccdf3d50ff9bc4408940e182
Jose Antonio Solis Martinez <jose-antonio.solis@atos.net>
2022-09-21 16:04:40 -0500

    add functionality of create account page

--- src/main/java/com/finalproject/qa/pageobjects/CreateAccountPage.java
+++ src/main/java/com/finalproject/qa/pageobjects/CreateAccountPage.java
@@ -14,4 +14,42 @@ public class CreateAccountPage extends CommonElements {
         PageFactory.initElements(driver, this);
     }
 
+    @FindBy(id = "firstname")
+    WebElement firstNameTextField;
+
+    @FindBy(id = "lastname")
+    WebElement lastNameTextField;
+
+    @FindBy(css = "#is_subscribed")
+    WebElement newsletterCheckbox;
+
+    @FindBy(css = "#email_address")
+    WebElement emailTextField;
+
+    @FindBy(css = "#password")
+    WebElement passwordTextField;
+
+    @FindBy(css = "#password-confirmation")
+    WebElement confirmPasswordTextField;
+
+    @FindBy(css = "#form-validate > div > div.primary > button")
+    WebElement createAccountButton;
+
+    public void addPersonalInformation(String firstName, String lastName, boolean signUpToNewsletter) {
+        firstNameTextField.sendKeys(firstName);
+        lastNameTextField.sendKeys(lastName);
+        if (signUpToNewsletter)
+            newsletterCheckbox.click();
+    }
+
+    public void addSignInInformation(String email, String password, String passwordConfirmation) {
+        emailTextField.sendKeys(email);
+        passwordTextField.sendKeys(password);
+        confirmPasswordTextField.sendKeys(passwordConfirmation);
+    }
+
+    public void clickCreateAccountButton() {
+        createAccountButton.click();
+    }
+
 }
